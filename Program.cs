3d1e9774ef16c6e996abc8d5011df28332417bdb
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-14 11:54:26 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double altura, pesoIdeal;
+string sexo;
+
+Console.Write("Digite sua altura em m..........: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Sexo [M]asculino / [F]eminino...: ");
+sexo = Console.ReadLine()!;
+
+if (sexo == "M")
+{
+    pesoIdeal = altura * 72.7 - 58.0;
+}
+else
+{
+    pesoIdeal = altura * 62.1 - 44.7;
+}
+
+Console.WriteLine($"Seu peso ideal é {pesoIdeal:N1}");
